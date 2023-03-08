af5142cd6c0661b1a8263da2117bd473f3b27330
johngil15 <jhongi.983@gmail.com>
2023-03-08 08:20:46 -0500

    Se trabaja en API de Gifs

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,19 +1,7 @@
+import { getGifs } from "../helpers/getGifs";
+
 export const GifGrid = ({ category }) => {
-  const getGifs = async() => {
-    const url = `api.giphy.com/v1/gifs/search?api_key=BbN4ATx2qrZzyxCcU60PEh12VNtcQqoe&q=${ category }}`;
-    const resp = await fetch( url );
-    const { data } = await resp.json();
-
-    console.log(resp.statusText);
-
-    // const gifs = data.map( img => ({
-    //   id: img.id,
-    //   title: img.title,
-    //   url: img.images
-    // }));
-  }
-
-  getGifs();
+  getGifs(category);
 
   return (
     <>
