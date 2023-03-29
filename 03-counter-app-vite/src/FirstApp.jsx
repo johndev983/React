4107f0b5422a2ee5fc73c8d0da6635528f66b217
John Gil <john.gil@comuniquemonos.com>
2023-03-29 09:22:22 -0500

    Se soluciona conflicto

---
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+
+export const FirstApp = ({ title, subTitle, name }) => {
+  return (
+    <>
+      <h1>{ title }</h1>
+      {/* <code>{ JSON.stringify(newMessage) }</code> */}
+      <p>{ subTitle }</p>
+      <p>{ name }</p>
+    </>
+  )
+}
+
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  subTitle: PropTypes.string.isRequired
+}
+
+FirstApp.defaultProps = {
+  name: 'John Gil',
+  subTitle: 'No hay subtitle',
+  // title: 'No hay titulo'
+}
