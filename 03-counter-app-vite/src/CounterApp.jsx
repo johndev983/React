048f58325923819fe1c0d521d5acbac87ea215a1
John Gil <john.gil@comuniquemonos.com>
2023-04-04 12:41:25 -0500

    Aprendemos a probar funciones y componentes

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -13,11 +13,11 @@ export const CounterApp = ({ value }) => {
   return (
     <>
       <h1>CounterApp</h1>
-      <h2> { counter } </h2>
+      <h2>{ counter }</h2>
 
       <button onClick={ handleAdd }>+1</button>
       <button onClick={ handleSubstract }>-1</button>
-      <button onClick={ handleReset }>Reset</button>
+      <button aria-label="btn-reset" onClick={ handleReset }>Reset</button>
     </>
   )
 }
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
-// import { CounterApp } from './CounterApp';
-import { FirstApp } from './FirstApp';
+import { CounterApp } from './CounterApp';
+// import { FirstApp } from './FirstApp';
 // import { HelloWorldApp } from './HelloWorldApp';
 
 import './styles.css'
 
 ReactDOM.createRoot( document.getElementById('root') ).render(
   <React.StrictMode>
-    {/* <CounterApp value={ 10 } /> */}
-    <FirstApp title="Hola soy Vegeta" />
+    <CounterApp value={ 10 } />
+    {/* <FirstApp title="Hola soy Vegeta" /> */}
   </React.StrictMode>
 )
