11204bf8531860d7a81bd26d1e165f0cd1f0b3d0
John Gil <john.gil@comuniquemonos.com>
2023-04-27 17:00:41 -0500

    Add: new directory 08-useReducer/

--- 04-gif-expert-app/test/components/GifGrid.test.jsx
+++ 04-gif-expert-app/test/components/GifGrid.test.jsx
@@ -1,16 +1,43 @@
-import { render, screen } from "@testing-library/react"
-import { GifGrid } from "../../src/components/GifGrid";
+import { render, screen } from '@testing-library/react'
+import { GifGrid } from '../../src/components/GifGrid';
+import { useFetchGifs } from '../../src/hooks/useFetchGifs';
+
+jest.mock('../../src/hooks/useFetchGifs');
 
 describe('Pruebas en <GifGrid />', () => {
   const category = 'One Punch'
 
   test('Debe de mostrar el loading inicialmente', () => {
+    useFetchGifs.mockReturnValue({
+      images: [],
+      isLoading: true
+    });
+
     render( <GifGrid category={ category } /> );
     expect( screen.getByText( 'Cargando...' ) );
     expect( screen.getByText( category ) );
   })
 
   test('Debe de mostrar items cuando se cargan las imagenes useFetchGifs', () => {
-    // Queda en veremos
+    const gifs = [
+      {
+        id: 'ABC',
+        title: 'Saitama',
+        url: 'https://localhost/saitama.jpg'
+      },
+      {
+        id: '123',
+        title: 'Goku',
+        url: 'https://localhost/goku.jpg'
+      }
+    ]
+    
+    useFetchGifs.mockReturnValue({
+      images: gifs,
+      isLoading: false
+    });
+
+    render( <GifGrid category={ category } /> );
+    expect( screen.getAllByRole('img').length ).toBe(2);
   })
 })
--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { CallBackHook } from './06-memos/CallBackHook';
+import { TodoApp } from './08-useReducer/TodoApp';
+// import './08-useReducer/intro-reducer';
+// import { Padre } from './07-tarea-memo/Padre';
+// import { CallBackHook } from './06-memos/CallBackHook';
 // import { MemoHook } from './06-memos/MemoHook';
 // import { Memorize } from './06-memos/Memorize';
 // import { Layout } from './05-useLayoutEffect/Layout';
@@ -16,6 +19,6 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <CallBackHook />
+    <TodoApp />
   // </React.StrictMode>,
 )
